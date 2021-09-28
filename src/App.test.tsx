44aa0c74e9eeaa62d42837793e9d9c7a778e5698
tj-ubuntu <abongsjoel@gmail.com>
2021-09-28 19:33:03 +0100

    Implement before each hook

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,20 +1,21 @@
-import React from "react";
-import { render } from "@testing-library/react";
 import App from "./App";
 
-import { configure, shallow } from "enzyme";
+import { configure, shallow, ShallowWrapper } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 
 configure({ adapter: new Adapter() });
 
 describe("Counter Testing", () => {
+  let wrapper: ShallowWrapper<any, Readonly<{}>, React.Component<{}, {}, any>>;
+
+  beforeEach(() => {
+    wrapper = shallow(<App />);
+  });
   test("renders the title of counter", () => {
-    const wrapper = shallow(<App />);
     expect(wrapper.find("h1").text()).toContain("This is the counter app");
   });
 
   test("render a button with a text of 'increment'", () => {
-    const wrapper = shallow(<App />);
     expect(wrapper.find("#increment-btn").text()).toBe("Increment");
   });
 });

12a9b68386a830ab8f029280033090fa22693733
tj-ubuntu <abongsjoel@gmail.com>
2021-09-28 19:22:15 +0100

    First test passes

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,15 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import { configure, shallow } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+
+configure({ adapter: new Adapter() });
+
+describe("Counter Testing", () => {
+  test("renders the title of counter", () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find("h1").text()).toContain("This is the counter app");
+  });
 });
